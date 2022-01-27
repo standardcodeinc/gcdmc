3d4916dc1cb45a317bfbaba4079ef9aab6af13ac
Daniel Hung <dhung09@gmail.com>
2022-01-26 20:52:29 -0800

    tests for key list

--- tests/test_key_list.py
+++ tests/test_key_list.py
@@ -1,80 +1,85 @@
 from __future__ import annotations
 
+import mock
 import pytest
 
+from google.api_core.client_options import ClientOptions
+from google.auth.credentials import Credentials
 from google.cloud.datastore import Client, Key
 
 from gcdmc.model.types import KeyList
 
-c: Client = Client(project='test-project')
+c: Client = Client(project='test-project',
+                   namespace='test-namespace',
+                   credentials=mock.Mock(spec=Credentials))
 
 
-def key() -> Key:
-    return c.key()
+def key(name: str) -> Key:
+    return c.key('test-kind', name)
 
 
 def test_init() -> None:
-    l: KeyList = KeyList(['a', 'b'])
-    assert l == ['a', 'b']
+    l: KeyList = KeyList([key('a'), key('b')])
+    assert l == [key('a'), key('b')]
 
 
 def test_append() -> None:
     l: KeyList = KeyList()
-    l.append('a')
-    l.append('b')
-    assert l == ['a', 'b']
+    l.append(key('a'))
+    l.append(key('b'))
+    assert l == [key('a'), key('b')]
 
 
 def test_extend() -> None:
     l: KeyList = KeyList()
-    l.extend(['a', 'b'])
-    assert l == ['a', 'b']
+    l.extend([key('a'), key('b')])
+    assert l == [key('a'), key('b')]
 
 
 def test_add_regular_list() -> None:
-    l: KeyList = KeyList(['a', 'b'])
-    result: KeyList = l + ['c', 'd']
+    l: KeyList = KeyList([key('a'), key('b')])
+    result: KeyList = l + [key('c'), key('d')]
     assert isinstance(result, KeyList)
-    assert result == ['a', 'b', 'c', 'd']
+    assert result == [key('a'), key('b'), key('c'), key('d')]
 
 
 def test_add_key_list() -> None:
-    l: KeyList = KeyList(['a', 'b'])
-    result: KeyList = l + KeyList(['c', 'd'])
+    l: KeyList = KeyList([key('a'), key('b')])
+    result: KeyList = l + KeyList([key('c'), key('d')])
     assert isinstance(result, KeyList)
-    assert result == ['a', 'b', 'c', 'd']
+    assert result == [key('a'), key('b'), key('c'), key('d')]
 
 
 def test_iadd_regular_list() -> None:
-    l: KeyList = KeyList(['a', 'b'])
-    l += ['c', 'd']
-    assert l == ['a', 'b', 'c', 'd']
+    l: KeyList = KeyList([key('a'), key('b')])
+    l += [key('c'), key('d')]
+    assert l == [key('a'), key('b'), key('c'), key('d')]
 
 
 def test_iadd_key_list() -> None:
-    l: KeyList = KeyList(['a', 'b'])
-    l += KeyList(['c', 'd'])
-    assert l == ['a', 'b', 'c', 'd']
+    l: KeyList = KeyList([key('a'), key('b')])
+    l += KeyList([key('c'), key('d')])
+    assert l == [key('a'), key('b'), key('c'), key('d')]
 
 
 def test_insert() -> None:
     l: KeyList = KeyList()
-    l.insert(0, 'a')
-    l.insert(1, 'b')
-    assert l == ['a', 'b']
+    l.insert(0, key('a'))
+    l.insert(1, key('b'))
+    assert l == [key('a'), key('b')]
 
 
 def test_copy_retains_type_and_values() -> None:
-    l: KeyList = KeyList(['a', 'b'])
+    l: KeyList = KeyList([key('a'), key('b')])
     copy: KeyList = l.copy()
     assert isinstance(copy, KeyList)
     assert l is not copy
-    assert copy == ['a', 'b']
+    assert copy == [key('a'), key('b')]
 
 
 def test_init_invalid_type() -> None:
     with pytest.raises(TypeError):
-        _: KeyList = KeyList(['a', 1])
+        _: KeyList = KeyList([key('a'), 1])
 
 
 def test_append_invalid_type() -> None:
@@ -86,19 +91,19 @@ def test_append_invalid_type() -> None:
 def test_extend_invalid_type() -> None:
     l: KeyList = KeyList()
     with pytest.raises(TypeError):
-        l.extend(['a', 1])
+        l.extend([key('a'), 1])
 
 
 def test_add_invalid_type() -> None:
     l: KeyList = KeyList()
     with pytest.raises(TypeError):
-        _: KeyList = l + ['a', 1]
+        _: KeyList = l + [key('a'), 1]
 
 
 def test_iadd_invalid_type() -> None:
     l: KeyList = KeyList()
     with pytest.raises(TypeError):
-        l += ['a', 1]
+        l += [key('a'), 1]
 
 
 def test_insert_invalid_type() -> None:
